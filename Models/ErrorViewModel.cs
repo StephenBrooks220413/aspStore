94cda401f0e6d9847c2a49b7b6b48b2b935f0ec2
Stephen A Brooks <krammer439298@gmail.com>
2021-08-21 18:42:48 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
